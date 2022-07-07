e588520ac99acee34151e2f71aad4cd7bd6d7280
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2022-07-07 11:38:54 +0200

    modification in config

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -51,6 +51,7 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
+# /opt/tuxedo-control-center/tuxedo-control-center &
 picom --config $HOME/.config/qtile/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 # /usr/lib/xfce4/notifyd/xfce4-notifyd &
